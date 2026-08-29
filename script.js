0abc728c2383edb0c5ba0c4b0d6f312f394d006e
AriK682 <ridgleyak@vcu.edu>
2026-08-29 16:21:46 -0400

    add a keyboard press

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 //create an array of sounds
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong','christmas','bamboo','siren'];
+
+//create an array of keys that correspond to the sounds
+const keys = ['a', 's', 'd', 'f', 'g', 'h','j','k','l'];
 
 //for each sound in the array, create a button,update text, and append to DOM
 sounds.forEach((sound) => {
@@ -9,6 +12,37 @@ sounds.forEach((sound) => {
     btn.classList.add('btn');
     //set the text of the button to the sound name
     btn.innerText = sound;
+
+    //add an event listener to the button that plays the sound when clicked
+    btn.addEventListener('click', () => {
+        //stop sounds first
+        stopSounds();
+        document.getElementById(sound).play();
+    });
+
+    
     //add the button to the DOM
     document.getElementById('buttons').appendChild(btn);
 });
+
+//add an event listener to the button that plays the sound when a key is pressed
+//made it similar to the piano project
+document.addEventListener('keydown', (e) => {
+    const key = e.key.toLowerCase();
+    const index = keys.indexOf(key);
+        //if the key is in the array, play the sound
+    if (index !== -1 && index < sounds.length) {
+        stopSounds();
+        document.getElementById(sounds[index]).play();
+    }
+});
+
+//Helper function
+function stopSounds() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0;
+    });
+}
